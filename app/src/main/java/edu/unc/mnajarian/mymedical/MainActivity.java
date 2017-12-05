ffe57005ca90bd7891846458621200243074e525
Maya Najarian <maya.najarian@gmail.com>
2017-12-05 01:17:26 -0500

    Files for search records activity

--- app/src/main/java/edu/unc/mnajarian/mymedical/MainActivity.java
+++ app/src/main/java/edu/unc/mnajarian/mymedical/MainActivity.java
@@ -82,17 +82,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         if (requestCode == 1 && resultCode == RESULT_OK) {
-
             Intent x = new Intent(this, CaptureImage.class);
             x.putExtra("captureUri", currentPicUri.toString()); //key, value
             x.putExtra("db", db.toString());
             startActivity(x);
         }
+    }
+
+    public void searchRecords(View v){
+        Intent x = new Intent(this, SearchRecords.class);
+
     }
 
 
---
+++ app/src/main/java/edu/unc/mnajarian/mymedical/SearchRecords.java
@@ -0,0 +1,21 @@
+package edu.unc.mnajarian.mymedical;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mnajarian on 12/5/17.
+ */
+
+public class SearchRecords extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_records);
+
+    }
+
+
+
+}
